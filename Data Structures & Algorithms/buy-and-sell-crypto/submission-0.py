0def6c7437d0523d56fde319b15fba6c3c795192
Ankita Chakrawarti <80532826+ankita-2021@users.noreply.github.com>
2026-04-19 08:37:32 +0530

    Remove maxProfit method from Solution class
    Remove the implementation of maxProfit method.

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,16 +1,1 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
-        profit  = 0
-        idx = 0
-        for i in range(1, len(prices)):
-            if prices[i] < buy:
-                buy = prices[i]
-                idx = i
-                break
-        sell = prices[idx]
-        for j in range(idx, len(prices)):
-            if prices[j] > sell:
-                sell = prices[j]
-        profit = sell - buy
-        return profit
+
