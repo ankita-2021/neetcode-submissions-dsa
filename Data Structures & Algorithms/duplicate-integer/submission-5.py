061478279da37efbf6782801891ae219987e5ae2
Ankita Chakrawarti <80532826+ankita-2021@users.noreply.github.com>
2026-04-19 23:15:16 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # is_true = False
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if(nums[i]==nums[j]):
+        #             is_true = True
+        #             break
+        # return is_true
+        # agar arr sorted ho tb nhi toh two pointer will not work
+        # nums.sort()
+        # i = 0
+        # j = 1
+        # while j < len(nums):
+        #     if nums[i] == nums[j]:
+        #         return True
+        #     i += 1
+        #     j += 1
+        # return False
+
+        # # best optimal hashmap hashset
+        # seen = set()
+        # for i in range(len(nums)):
+        #     if nums[i] in seen:
+        #         return True
+        #     seen.add(nums[i])
+        # return False
+
+        freq = {}
+        for num in nums:
+            if num in freq:
+                return True
+            freq[num] = 1
+        return False
